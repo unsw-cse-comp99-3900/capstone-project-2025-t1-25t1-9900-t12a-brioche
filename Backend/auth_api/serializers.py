54aeba17ec5aea9fd4c6f4f1b40b077dbbc7f7c9
YueYao-Simba <yaoyue_2024@163.com>
2025-04-15 19:24:35 +1000

    Add greeting field to RegisterSerializer (by Yueyao)

--- Backend/auth_api/serializers.py
+++ Backend/auth_api/serializers.py
@@ -3,17 +3,24 @@ from rest_framework import serializers
 
 User = get_user_model()
 
+
 class RegisterSerializer(serializers.ModelSerializer):
+    greeting = serializers.SerializerMethodField()  
+
     class Meta:
         model = User
-        fields = ["email", "password"]
+        fields = ["email", "password", "greeting"]  
         extra_kwargs = {
             "password": {"write_only": True},
         }
 
+    def get_greeting(self, obj):
+        return f"Welcome, your email is {obj.email}! - from Yueyao"  
+
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
