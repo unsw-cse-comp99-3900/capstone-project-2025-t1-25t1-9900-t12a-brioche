5e3a811f28a10c6c0e17672d3825a5ea101938b1
Wadubawang <764351768@qq.com>
2025-04-20 14:40:50 +1000

    Add files via upload

--- Frontend/sdg/src/routes.jsx
+++ Frontend/sdg/src/routes.jsx
@@ -2,6 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ResetPassword from "./pages/ResetPassword";
+import UserProfile from "./pages/UserProfile";
+import GroupProfile from "./pages/GroupProfile";  
+import MainPage from "./pages/MainPage";  
+import SearchResults from './pages/SearchResults';
+import ContentDetail from './pages/ContentDetail';
+
+
+
 
 function AppRoutes() {
   return (
@@ -11,6 +19,11 @@ function AppRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/profile" element={<UserProfile />} />
+      <Route path="/group" element={<GroupProfile />} />  // 👈 新增
+      <Route path="/main" element={<MainPage />} />
+      <Route path="/search" element={<SearchResults />} />
+      <Route path="/content/:id" element={<ContentDetail />} />
     </Routes>
   );
 }
